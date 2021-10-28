60190921ac397ecf53fa0b1a51f76b9ae68bb34e
Jordan Smith <smithjordan130@icloud.com>
2021-10-28 15:08:54 +0100

    Make a route and link to it

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,13 +8,9 @@
 </head>
 <body>
 	<article>
-		<h1><a href="/post">My First Post</h1></a>
+		<h1><a href="/post"></a>My First Post</h1>
 		<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate, rem. Nobis fugiat voluptates in laudantium voluptate ipsum. Quidem eos asperiores, praesentium deserunt sapiente corporis suscipit eveniet debitis dolor minus fugit?</p>
 	</article>
-
-	 <article>
-		<h1><a href="/post">My Second Post</h1></a>
-		<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate, rem. Nobis fugiat voluptates in laudantium voluptate ipsum. Quidem eos asperiores, praesentium deserunt sapiente corporis suscipit eveniet debitis dolor minus fugit?</p>
-	</article>
+	<a href="/">Go Back</a>
 </body>
 </html>
