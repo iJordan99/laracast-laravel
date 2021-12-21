f25f853056cb899ea8a6c00a403a92548b78462c
Jordan Smith <smithjordan130@icloud.com>
2021-12-21 18:29:44 +0000

    Collection Sorting and Page Caching

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -25,15 +25,22 @@ class Post
 
 	public static function all()
 	{
-	 	return collect(File::files(resource_path("posts")))
-	 		->map(fn ($file) => YamlFrontMatter::parseFile($file))
-	 		->map(fn ($document) => new Post(
-	 			$document->title,
-	 			$document->excerpt,
-	 			$document->date,
-	 			$document->body(),
-	 			$document->slug
-	 		));
+		// Tinker
+		// cache()->forget('post.all'); ** Clears the cache
+		// cache()->put('foo','bar');
+		//cache('foo');
+		return cache()->rememberForever('posts.all', function () {
+			return collect(File::files(resource_path("posts")))
+		 		->map(fn ($file) => YamlFrontMatter::parseFile($file))
+		 		->map(fn ($document) => new Post(
+		 			$document->title,
+		 			$document->excerpt,
+		 			$document->date,
+		 			$document->body(),
+		 			$document->slug
+	 		))
+			->sortByDesc('date');
+	 	})
 	}
 	public static function find($slug)
 	{
