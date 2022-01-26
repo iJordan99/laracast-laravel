c41290de4c3284216320988706db981a031b5aeb
Jordan Smith <smithjordan130@icloud.com>
2022-01-26 13:29:35 +0000

    Mass assignment vulnerabilities

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // attributes that cant be mass assigned
+    // protected $guarded = []
+
+    // attributes that can be mass assigned
+    protected $fillable = [
+        'title',
+        'excerpt',
+        'body',
+    ];
 }
